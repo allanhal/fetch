22c8c3ec9295f0fd0107dc9eda31fc11c537fa99
allan <allan.hal@gmail.com>
2022-10-01 17:05:19 -0300

    fetch post

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ function retornoPromessaFetch(resposta) {
 }
 
 function retornoPromessaBody(body) {
-    body.forEach(function (pessoa) {
-      console.log(pessoa);
-      lista.innerHTML = lista.innerHTML + "<li>" + pessoa.nome + "</li>";
-    });
+  lista.innerHTML = "";
+  body.forEach(function (pessoa) {
+    lista.innerHTML = lista.innerHTML + "<li>" + pessoa.nome + "</li>";
+  });
+}
+function atualizar() {
+  let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa");
+  promessa.then(retornoPromessaFetch).then(retornoPromessaBody);
 }
 
-let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa");
-
-promessa.then(retornoPromessaFetch).then(retornoPromessaBody);
+function adicionar() {
+  let nomeDaPessoa = nome.value;
+  console.log(nomeDaPessoa);
+  let requestPost = {
+    method: "POST",
+    // ato de fé
+    headers: {
+      "content-type": "application/json",
+    },
+    // conteudo que eu desejo enviar
+    body: JSON.stringify({
+      idade: 20,
+      nome: nomeDaPessoa,
+    }),
+  };
+  fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa", requestPost).then(
+    function (resposta) {
+      console.log(resposta);
+    }
+  );
+}
