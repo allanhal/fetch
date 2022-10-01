c9d6ffce74bea7402d0d6ab6eb0717e8f8c70780
allan <allan.hal@gmail.com>
2022-10-01 14:54:37 -0300

    fetch abreviado

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ let promessa2 = promessa1.then(retornoPromessa1); //promessa2
 console.log(promessa2);
 promessa2.then(retornoPromessa2);
 
-fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa")
-  .then(function (response) {
-    //magica => transforma o body da response em uma nova promessa
-    return response.json();
-  })
-  .then(function (body) {
-    console.log(body);
-  });
+// let generica;
+// fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa")
+//   .then(function (response) {
+//     //magica => transforma o body da response em uma nova promessa
+//     return response.json();
+//   })
+//   .then(function (body) {
+//     // body = corpo = conteudo json da response
+//     console.log("body", body);
+//     generica = body;
+//   });
+// console.log('generica', generica);
+
+// setTimeout(function(){
+//     console.log('generica no settimeout', generica)
+// }, 200)
